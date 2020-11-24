eb81581cbacd55fbe680a927b46bd4f0ce83f6e7
JinaKim77 <G00353420@gmit.ie>
2020-11-24 13:20:01 +0000

    Publish messages to Kafka and Consume messages from Kafka

--- kafka-consumer/src/main/java/Application.java
+++ kafka-consumer/src/main/java/Application.java
@@ -1,4 +1,10 @@
-import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.serialization.LongDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
 
 /**
  * Kafka Consumer Application
@@ -23,12 +29,37 @@ public class Application {
     }
 
     public static void consumeMessages(String topic, Consumer<Long, String> kafkaConsumer) {
-        // COMPLETE THIS METHOD
+        kafkaConsumer.subscribe(Collections.singletonList(topic));
+
+        while(true){
+            ConsumerRecords<Long, String> consumerRecords = kafkaConsumer.poll(Duration.ofSeconds(1));
+
+            if(consumerRecords.isEmpty()) {
+                //do something
+            }
+
+            for(ConsumerRecord<Long, String> record: consumerRecords){
+                System.out.println(String.format("Received record (key: %d, value: %s, partition: %d, offset: %d" ,
+                        record.key(), record.value(), record.partition(), record.offset()));
+            }
+
+            //do some processing
+
+            kafkaConsumer.commitAsync();
+        }
 
     }
 
     public Consumer<Long, String> createKafkaConsumer(String bootstrapServers, String consumerGroup) {
-        // COMPLETE THIS METHOD
+        Properties properties = new Properties();
+
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class.getName());
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG,consumerGroup);
+        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG,false);
+
+        return new KafkaConsumer<>(properties);
     }
 
 }
--- kafka-producer/src/main/java/Application.java
+++ kafka-producer/src/main/java/Application.java
@@ -1,5 +1,8 @@
-import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 
+import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
 /**
@@ -7,17 +10,68 @@ import java.util.concurrent.ExecutionException;
  */
 public class Application {
 
+    private static final String TOPIC ="events";
+    private static final String BOOTSTRAP_SERVER ="localhost:9092,localhost:9093,localhost:9094";
+
     public static void main(String[] args) {
         Application kafkaApplication = new Application();
-//        Producer<Long, String> kafkaProducer = kafkaApplication.createKafkaProducer(...);
+        Producer<Long, String> kafkaProducer = kafkaApplication.createKafkaProducer(BOOTSTRAP_SERVER);
+
+        //Ctrl + Alt + T
+        try {
+            produceMessages(10, kafkaProducer);
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        } finally{
+            kafkaProducer.flush();
+            kafkaProducer.close();
+        }
     }
 
-    public void produceMessages(int numberOfMessages, Producer<Long, String> kafkaProducer) throws ExecutionException, InterruptedException {
+    public static void produceMessages(int numberOfMessages, Producer<Long, String> kafkaProducer) throws ExecutionException, InterruptedException {
+        //int partition = 0;  //specify a partition number
 
+        //to generate messages using different keys and values
+        for(int i = 0; i < numberOfMessages; i++){
+            //Record contains key,value and timestamp
+            long key = i;
+            String value = String.format("event %d", i);
+            long timestamp = System.currentTimeMillis();
+
+            //send directly to a particular partition
+            //--ProducerRecord<Long, String> record = new ProducerRecord(TOPIC, partition, timestamp, key, value);
+
+            //Use key, let kafka client library choose partition based on hash of key
+            //--ProducerRecord<Long, String> record = new ProducerRecord(TOPIC, key, value);
+
+            //left out key, messages sent using round-robin strategy
+            ProducerRecord<Long, String> record = new ProducerRecord(TOPIC, value);
+
+            //Send record to kafka and get result
+            RecordMetadata recordMetadata = kafkaProducer.send(record).get();
+
+            System.out.println(String.format("Record with (key: %s, value: %s) was sent to (partition: %d , offset: %d",
+                    record.key(), record.value(), recordMetadata.partition(), recordMetadata.offset()));
+        }
     }
 
-//    public Producer<Long, String> createKafkaProducer(String bootstrapServers) {
-//
-//    }
+                   //The key in our message is going to be long, and the values are going to be String
+    public Producer<Long, String> createKafkaProducer(String bootstrapServers) {
+        //--------To set up the basic configuration that I need for the kafka producer--------
 
+        //Create Properties object
+        Properties properties = new Properties();
+
+        //The location of the bootstrap server
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        //To give the produce a name
+        properties.put(ProducerConfig.CLIENT_ID_CONFIG, "events-producer");
+        //This will tell Kafka that key should be serialized
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
+        //This will tell Kafka that value should be serialized
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+
+        return new KafkaProducer<Long, String>(properties);
+    }
 }
